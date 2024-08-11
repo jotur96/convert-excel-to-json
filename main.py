c5c2b236c968683b4a6759d4a5bba6de968485c5
Julio Olmedo <turlan125@gmail.com>
2024-08-11 00:34:01 -0400

    feat: Add FastAPI endpoint for uploading Excel files and converting to JSON

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from typing import List
+
+import shutil
+
+from convert_json import ConvertToJson
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.post("/upload-excel/")
+async def upload_excel(file: UploadFile = File(...)):
+    # Guardar el archivo temporalmente
+    temp_file_path = f"./file/{file.filename}"
+    with open(temp_file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+        
+
+    converter = ConvertToJson()
+    
+    # Pasar el archivo a la función convert_json.main()
+    result = converter.convert(file_path=temp_file_path)
+    
+    # Devolver el resultado JSON
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
